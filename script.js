09410328e069678223ee688f0d4953c05365484f
Aaron Day <aaron.day@tlgcohort.com>
2023-12-14 14:10:45 -0800

    card creation complete

--- script.js
+++ script.js
@@ -5,10 +5,34 @@ user_input_form.addEventListener("submit", (e)  => {
 
     const destinationName = destination_name.value;
     const locationName = location_name.value;
-    const photoUrl = photo_url || PLACEHOLDER_PHOTO_URL;
+    const photoUrl = photo_url.value || PLACEHOLDER_PHOTO_URL;
     const desc = description.value;
 
     user_input_form.reset();
 
-    console.log(destinationName, locationName, photoUrl, desc);
+    const card = createCard(destinationName, locationName, photoUrl, desc);
+
+    cards_container.appendChild(card);
 });
+
+function createCard({destinationName, locationName, photoUrl, desc}) {
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("style", "width: 18rem");
+    card.innerHTML= `
+      <img class="card-img-top" src=${photoUrl} alt=${destinationName} at ${locationName}>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+        ${desc && `<p class="card-text">${desc}</p>`}
+    
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <button type="button" class="btn btn-info">Edit</button>
+        <button type="button" class="btn btn-danger">Delete</button>
+      </div>
+    `;
+
+    return card;
+}
