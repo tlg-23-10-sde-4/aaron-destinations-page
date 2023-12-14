c858fd85a56b81181e6f60b96f7e4d8956052049
Aaron Day <aaron.day@tlgcohort.com>
2023-12-14 14:47:30 -0800

    card creation actually complete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ user_input_form.addEventListener("submit", (e)  => {
     
     const PLACEHOLDER_PHOTO_URL = "https://plus.unsplash.com/premium_photo-1673264933459-808963ed1594?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bXQlMjByYWluaWVyfGVufDB8fDB8fHww";
 
+
     const destinationName = destination_name.value;
     const locationName = location_name.value;
     const photoUrl = photo_url.value || PLACEHOLDER_PHOTO_URL;
@@ -10,7 +11,7 @@ user_input_form.addEventListener("submit", (e)  => {
 
     user_input_form.reset();
 
-    const card = createCard(destinationName, locationName, photoUrl, desc);
+    const card = createCard({destinationName, locationName, photoUrl, desc});
 
     cards_container.appendChild(card);
 });
@@ -21,14 +22,13 @@ function createCard({destinationName, locationName, photoUrl, desc}) {
     card.classList.add("card");
     card.setAttribute("style", "width: 18rem");
     card.innerHTML= `
-      <img class="card-img-top" src=${photoUrl} alt=${destinationName} at ${locationName}>
+      <img src="${photoUrl}" class="card-img-top"  alt="${destinationName} at ${locationName}">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     
         ${desc && `<p class="card-text">${desc}</p>`}
     
-        <a href="#" class="btn btn-primary">Go somewhere</a>
         <button type="button" class="btn btn-info">Edit</button>
         <button type="button" class="btn btn-danger">Delete</button>
       </div>
